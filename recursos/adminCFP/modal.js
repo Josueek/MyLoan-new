2de77c4bd983391b8834eb56e52ba37d5ec501ea
LennynVides <lennyn.vides11@gmail.com>
2024-04-18 22:41:14 -0600

    lennyn termine mis pantalla

--- recursos/adminCFP/modal.js
+++ recursos/adminCFP/modal.js
@@ -20,3 +20,23 @@ window.onclick = function(event) {
 document.getElementById('buscarEspacio').addEventListener('click', function() {
     openModal();
 });
+
+// Función para abrir el modal
+function openModal2() {
+    var modal = document.getElementById("modalObservacion");
+    modal.style.display = "block";
+  }
+
+  // Función para cerrar el modal
+  function closeModal2() {
+    var modal = document.getElementById("modalObservacion");
+    modal.style.display = "none";
+  }
+
+  // Cierra el modal si el usuario hace clic fuera de él
+  window.onclick = function(event) {
+    var modal = document.getElementById("modalObservacion");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
