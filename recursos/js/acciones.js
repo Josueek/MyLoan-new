9d3483251307929176aaf4f8298edb3c7ebb1036
Josueek <abdelmario351@gmail.com>
2024-04-22 11:05:24 -0600

    Inicio del proyecto estructurado para empezar a trabajar

---
+++ recursos/js/acciones.js
@@ -0,0 +1,66 @@
+//Funcion del menu lateral para que no se cierre el sub menu
+// Agregamos un evento de clic al enlace del menú de administrar para evitar que se cierre automáticamente
+document.getElementById('btnAdministrar').addEventListener('click', function (e) {
+    e.preventDefault(); // Evitamos que el enlace siga su acción predeterminada
+    e.stopPropagation(); // Evitamos que el clic se propague y cierre el menú
+    const submenu = document.getElementById('submenu');
+    setTimeout(() => {
+        submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block'; // Alternamos entre mostrar y ocultar el submenú
+    }, 100); // Esperamos 100 milisegundos antes de cambiar el estilo
+});
+
+//Control de desplazamiento de submenus
+
+// Función para controlar la apertura y cierre de los submenús
+function toggleSubmenu(submenuId) {
+    const submenu = document.getElementById(submenuId);
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+}
+
+// Agregar eventos de clic al enlace del menú de observaciones
+document.getElementById('btnobser').addEventListener('click', function (e) {
+    e.preventDefault(); // Evitar que el enlace siga su acción predeterminada
+    const submenu = document.getElementById('submenu7');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+});
+
+// Agregar eventos de clic al enlace del menú de reportes
+document.getElementById('btnReporte').addEventListener('click', function (e) {
+    e.preventDefault(); // Evitar que el enlace siga su acción predeterminada
+    const submenu = document.getElementById('submenu8');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+});
+
+// Agregar eventos de clic al enlace del menú de cursos
+document.getElementById('btnCursos').addEventListener('click', function (e) {
+    e.preventDefault(); // Evitar que el enlace siga su acción predeterminada
+    const submenu = document.getElementById('submenu9');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+});
+
+// Agregar eventos de clic al enlace del menú de mis prestamos
+document.getElementById('btnPrestamos').addEventListener('click', function (e) {
+    e.preventDefault(); // Evitar que el enlace siga su acción predeterminada
+    const submenu = document.getElementById('submenu10');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+});
