10153bbad24083177e0df654cb1bb7692b89562e
Brandon Daniel Sánchez Santamaría <brndonsantamaria@gmail.com>
2024-04-25 14:11:15 -0600

    Pantalla terminada
    Pantalla terminada de registro. todas validadas y responsivas

--- controlador/admin_itr/registroContinuacion.js
+++ controlador/admin_itr/registroContinuacion.js
@@ -18,3 +18,60 @@ $(document).ready(function () {
       }).click();
     });
   });  
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var confirmarBtn = document.getElementById('btn-confirmar');
+
+    confirmarBtn.addEventListener('click', function(event) {
+        event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+
+        var nombre = document.getElementById('inputNombre').value.trim();
+        var apellidos = document.getElementById('inputApellidos').value.trim();
+        var telefono = document.getElementById('inputTelefono').value.trim();
+
+        // Validar nombre
+        if (!nombre) {
+            mostrarError('Por favor, ingresa tu nombre.');
+            return;
+        }
+
+        // Validar apellidos
+        if (!apellidos) {
+            mostrarError('Por favor, ingresa tus apellidos.');
+            return;
+        }
+
+        // Validar teléfono
+        if (!telefono) {
+            mostrarError('Por favor, ingresa tu número de teléfono.');
+            return;
+        } else if (!/^\d+$/.test(telefono)) {
+            mostrarError('Por favor, ingresa un número de teléfono válido.');
+            return;
+        }
+
+        // Mostrar modal de registro completado con éxito
+        Swal.fire({
+            icon: 'success',
+            title: 'Registro completado',
+            text: 'Tu registro se ha completado con éxito.',
+            confirmButtonText: 'Ir al inicio'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Redireccionar al usuario al index.html
+                window.location.href = 'index.html';
+            }
+        });
+    });
+
+    // Función para mostrar una alerta de error
+    function mostrarError(mensaje) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: mensaje,
+        });
+    }
+});
