c3a3d8816b3ff7546b27df6380777ed3af3df692
dylanxs34r <103667099+Dylanxs@users.noreply.github.com>
2024-04-26 14:11:42 -0600

    Crear solicitud
    Menu para crear la solictud ya, faltan los modales de cada uno

---
+++ recursos/js/crear_solicitud_prestamo.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#fechaEntrega').datepicker({
+      format: 'dd/mm/yyyy',
+      autoclose: true
+    });
+
+    $('#calendarioBoton').click(function() {
+      $('#fechaEntrega').datepicker('show');
+    });
+  });
+
+
+    document.getElementById("btnAgregarequipo").addEventListener("click", function() {
+        // Redirecciona a la página deseada
+        window.location.href = "crear_solicitud_prestamousar.html";
+    });
