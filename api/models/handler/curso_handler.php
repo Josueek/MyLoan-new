d8387170ddaa39c1c47e096d956cb8bce61971e1
Brandon Daniel Sánchez Santamaría <brndonsantamaria@gmail.com>
2024-06-24 12:42:14 -0600

    SCRUD/CURSOS
    SCRUD terminado al 90% solo faltan validaciones.

---
+++ api/models/handler/curso_handler.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once('../helpers/database.php');
+require_once('../helpers/validator.php');
+
+class CursoHandler
+{
+    public function getAllCursos($buscar = '')
+    {
+        $sql = 'SELECT c.id_curso, c.nombre_curso, c.fecha_inicio, c.fecha_fin, c.cantidad_personas, c.grupo, c.programa_formacion, c.codigo_curso, c.id_empleado, e.nombre_empleado, c.estado
+                FROM tb_cursos c
+                LEFT JOIN tb_datos_empleados e ON c.id_empleado = e.id_datos_empleado
+                WHERE c.nombre_curso LIKE ?';
+        $params = ["%$buscar%"];
+        $data = Database::getRows($sql, $params);
+        if ($data) {
+            return array('status' => 1, 'dataset' => $data);
+        } else {
+            return array('status' => 0, 'message' => 'No se encontraron registros');
+        }
+    }
+
+    public function getAllEmpleados()
+    {
+        $sql = 'SELECT id_datos_empleado, nombre_empleado FROM tb_datos_empleados';
+        return Database::getRows($sql);
+    }
+
+    public function addCurso($params)
+    {
+        $sql = 'INSERT INTO tb_cursos (nombre_curso, fecha_inicio, fecha_fin, cantidad_personas, grupo, programa_formacion, codigo_curso, id_empleado, estado) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        return Database::executeRow($sql, $params);
+    }
+
+    public function getCursoById($idCurso)
+    {
+        $sql = 'SELECT * FROM tb_cursos WHERE id_curso = ?';
+        $params = array($idCurso);
+        return Database::getRow($sql, $params);
+    }
+
+    public function updateCurso($params)
+    {
+        $sql = 'UPDATE tb_cursos 
+                SET nombre_curso = ?, fecha_inicio = ?, fecha_fin = ?, cantidad_personas = ?, grupo = ?, programa_formacion = ?, codigo_curso = ?, id_empleado = ?, estado = ? 
+                WHERE id_curso = ?';
+        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteCurso($idCurso)
+    {
+        $sql = 'DELETE FROM tb_cursos WHERE id_curso = ?';
+        $params = array($idCurso);
+        return Database::executeRow($sql, $params);
+    }
+}
+?>
---
+++ api/services/curso_services.php
@@ -0,0 +1,127 @@
+<?php
+require_once('../helpers/database.php');
+require_once('../helpers/validator.php');
+require_once('../models/data/curso_data.php');
+
+if (isset($_GET['action'])) {
+    session_start();
+    $curso = new CursoData();
+    $result = array('status' => 0, 'message' => null, 'dataset' => null);
+
+    switch ($_GET['action']) {
+        case 'getAllCursos':
+            $buscar = isset($_GET['buscar']) ? $_GET['buscar'] : '';
+            $result = $curso->getAllCursos($buscar);
+            break;
+
+        case 'getAllEmpleados':
+            $result = array('status' => 1, 'dataset' => $curso->getAllEmpleados());
+            break;
+
+        case 'addCurso':
+            $_POST = Validator::validateForm($_POST);
+            if (isset($_POST['nombre']) &&
+                isset($_POST['fechaInicio']) &&
+                isset($_POST['fechaFin']) &&
+                isset($_POST['cantidadPersonas']) &&
+                isset($_POST['grupo']) &&
+                isset($_POST['programaFormacion']) &&
+                isset($_POST['codigoCurso']) &&
+                isset($_POST['empleado'])) {
+
+                if ($curso->setNombre($_POST['nombre']) &&
+                    $curso->setFechaInicio($_POST['fechaInicio']) &&
+                    $curso->setFechaFin($_POST['fechaFin']) &&
+                    $curso->setCantidadPersonas($_POST['cantidadPersonas']) &&
+                    $curso->setGrupo($_POST['grupo']) &&
+                    $curso->setProgramaFormacion($_POST['programaFormacion']) &&
+                    $curso->setCodigo($_POST['codigoCurso']) &&
+                    $curso->setEmpleado($_POST['empleado']) &&
+                    $curso->setEstado('pendiente')) {
+
+                    if ($curso->create()) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Curso agregado correctamente';
+                    } else {
+                        $result['message'] = 'No se pudo agregar el curso';
+                    }
+                } else {
+                    $result['message'] = 'Datos inválidos';
+                }
+            } else {
+                $result['message'] = 'Datos incompletos';
+            }
+            break;
+
+        case 'getCurso':
+            if (isset($_GET['id']) && Validator::validateNaturalNumber($_GET['id'])) {
+                if ($result['dataset'] = $curso->getCursoById($_GET['id'])) {
+                    $result['status'] = 1;
+                } else {
+                    $result['message'] = 'No se pudieron obtener los datos del curso';
+                }
+            } else {
+                $result['message'] = 'Datos inválidos';
+            }
+            break;
+
+        case 'updateCurso':
+            $_POST = Validator::validateForm($_POST);
+            if (isset($_POST['id']) &&
+                isset($_POST['nombre']) &&
+                isset($_POST['fechaInicio']) &&
+                isset($_POST['fechaFin']) &&
+                isset($_POST['cantidadPersonas']) &&
+                isset($_POST['grupo']) &&
+                isset($_POST['programaFormacion']) &&
+                isset($_POST['codigoCurso']) &&
+                isset($_POST['empleado']) &&
+                isset($_POST['estado'])) {
+
+                if ($curso->setId($_POST['id']) &&
+                    $curso->setNombre($_POST['nombre']) &&
+                    $curso->setFechaInicio($_POST['fechaInicio']) &&
+                    $curso->setFechaFin($_POST['fechaFin']) &&
+                    $curso->setCantidadPersonas($_POST['cantidadPersonas']) &&
+                    $curso->setGrupo($_POST['grupo']) &&
+                    $curso->setProgramaFormacion($_POST['programaFormacion']) &&
+                    $curso->setCodigo($_POST['codigoCurso']) &&
+                    $curso->setEmpleado($_POST['empleado']) &&
+                    $curso->setEstado($_POST['estado'])) {
+
+                    if ($curso->update()) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Curso actualizado correctamente';
+                    } else {
+                        $result['message'] = 'No se pudo actualizar el curso';
+                    }
+                } else {
+                    $result['message'] = 'Datos inválidos';
+                }
+            } else {
+                $result['message'] = 'Datos incompletos';
+            }
+            break;
+
+        case 'deleteCurso':
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (isset($data['id']) && Validator::validateNaturalNumber($data['id'])) {
+                if ($curso->delete($data['id'])) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Curso eliminado correctamente';
+                } else {
+                    $result['message'] = 'No se pudo eliminar el curso';
+                }
+            } else {
+                $result['message'] = 'Datos inválidos';
+            }
+            break;
+
+        default:
+            $result['message'] = 'Acción no disponible';
+    }
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($result);
+}
+?>
