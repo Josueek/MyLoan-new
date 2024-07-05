0ca9c8323573080a2d7c9538bc5dc0dfe6b27cf3
Josue Ortiz <20220329@ricaldone.edu.sv>
2024-07-05 11:01:36 -0600

    Datos de php

--- api/models/handler/curso_handler.php
+++ api/models/handler/curso_handler.php
@@ -1,17 +1,20 @@
 <?php
 
-require_once('../helpers/database.php');
-require_once('../helpers/validator.php');
+require_once ('../helpers/database.php'); // Incluye el archivo de conexión a la base de datos
+require_once ('../helpers/validator.php');
 
 class CursoHandler
 {
+
+    //Metodo que consulta los datos de la tabla de tb_Cursos y obtiene datos espscificos mediente
+    //UN filtro
     public function getAllCursos($buscar = '')
     {
         $sql = 'SELECT c.id_curso, c.nombre_curso, c.fecha_inicio, c.fecha_fin, c.cantidad_personas, c.grupo, c.programa_formacion, c.codigo_curso, c.id_empleado, e.nombre_empleado, c.estado
                 FROM tb_cursos c
                 LEFT JOIN tb_datos_empleados e ON c.id_empleado = e.id_datos_empleado
                 WHERE c.nombre_curso LIKE ?';
-        $params = ["%$buscar%"];
+        $params = ["%$buscar%"]; // Consulta SQL para obtener cursos con búsqueda
         $data = Database::getRows($sql, $params);
         if ($data) {
             return array('status' => 1, 'dataset' => $data);
@@ -20,12 +23,14 @@ class CursoHandler
         }
     }
 
+    //Metodo para obtener los datos de empleados
     public function getAllEmpleados()
     {
         $sql = 'SELECT id_datos_empleado, nombre_empleado FROM tb_datos_empleados';
         return Database::getRows($sql);
     }
 
+    //Metodo para agregar un nuevo curso
     public function addCurso($params)
     {
         $sql = 'INSERT INTO tb_cursos (nombre_curso, fecha_inicio, fecha_fin, cantidad_personas, grupo, programa_formacion, codigo_curso, id_empleado, estado) 
@@ -33,6 +38,8 @@ class CursoHandler
         return Database::executeRow($sql, $params);
     }
 
+
+    //Metodo para obtener los datos de un curso mediante el id
     public function getCursoById($idCurso)
     {
         $sql = 'SELECT * FROM tb_cursos WHERE id_curso = ?';
@@ -40,6 +47,7 @@ class CursoHandler
         return Database::getRow($sql, $params);
     }
 
+    //Actualizar los datos d la tabla cursos 
     public function updateCurso($params)
     {
         $sql = 'UPDATE tb_cursos 
@@ -48,6 +56,7 @@ class CursoHandler
         return Database::executeRow($sql, $params);
     }
 
+    //Borrar datos de la tabla de cursos
     public function deleteCurso($idCurso)
     {
         $sql = 'DELETE FROM tb_cursos WHERE id_curso = ?';
