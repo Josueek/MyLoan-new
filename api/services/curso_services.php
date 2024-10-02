ca60b440f50294c006719414fe0b4ca702c563ae
dylanxs34r <103667099+Dylanxs@users.noreply.github.com>
2024-10-02 09:47:26 -0600

    curso funcionando

--- api/services/curso_services.php
+++ api/services/curso_services.php
@@ -6,7 +6,7 @@ require_once('../models/data/curso_data.php');
 if (isset($_GET['action'])) {
     session_start();
     $curso = new CursoData();
-    $result = array('status' => 0, 'message' => null, 'dataset' => null, 'error' => null);
+    $result = array('status' => 0, 'message' => null, 'dataset' => null);
 
     switch ($_GET['action']) {
         case 'getAllCursos':
@@ -16,39 +16,20 @@ if (isset($_GET['action'])) {
 
         case 'getAllEmpleados':
             $result = array('status' => 1, 'dataset' => $curso->getAllEmpleados());
-
-        //Obtenemos los programas de formacion
-        case 'getProgramaFormacion':
-            if ($result['dataset'] = $curso->getPrograma()) {
-                $result['status'] = 1;
-            } else {
-                $result['message'] = 'No se pudieron obtener los datos';
-            }
             break;
-        //Valor del estado
-        case 'getEstadoCurso':
-            if ($result['dataset'] = $curso->getEstadoCurso()) {
-                $result['status'] = 1;
-            } else {
-                $result['message'] = 'No se pudieron obtener los datos';
-            }
-            break;
-
 
         case 'addCurso':
             $_POST = Validator::validateForm($_POST);
-            if (
-                isset($_POST['nombre']) &&
+            if (isset($_POST['nombre']) &&
                 isset($_POST['fechaInicio']) &&
                 isset($_POST['fechaFin']) &&
                 isset($_POST['cantidadPersonas']) &&
                 isset($_POST['grupo']) &&
                 isset($_POST['programaFormacion']) &&
                 isset($_POST['codigoCurso']) &&
-                isset($_POST['empleado'])
-            ) {
-                if (
-                    $curso->setNombre($_POST['nombre']) &&
+                isset($_POST['empleado'])) {
+
+                if ($curso->setNombre($_POST['nombre']) &&
                     $curso->setFechaInicio($_POST['fechaInicio']) &&
                     $curso->setFechaFin($_POST['fechaFin']) &&
                     $curso->setCantidadPersonas($_POST['cantidadPersonas']) &&
@@ -56,15 +37,13 @@ if (isset($_GET['action'])) {
                     $curso->setProgramaFormacion($_POST['programaFormacion']) &&
                     $curso->setCodigo($_POST['codigoCurso']) &&
                     $curso->setEmpleado($_POST['empleado']) &&
-                    $curso->setEstado('pendiente')
-                ) {
+                    $curso->setEstado('pendiente')) {
 
                     if ($curso->create()) {
                         $result['status'] = 1;
                         $result['message'] = 'Curso agregado correctamente';
                     } else {
                         $result['message'] = 'No se pudo agregar el curso';
-                        error_log(print_r($curso, true));
                     }
                 } else {
                     $result['message'] = 'Datos inválidos';
@@ -79,36 +58,7 @@ if (isset($_GET['action'])) {
                 if ($result['dataset'] = $curso->getCursoById($_GET['id'])) {
                     $result['status'] = 1;
                 } else {
-                    $result['message'] = 'No se obtuvo los datos del curso';
-                }
-            } else {
-                $result['message'] = 'Datos inválidos';
-            }
-            break;
-
-        /**
-         * Obtener los datos completos del curso
-         */
-        case 'getCursoCompleto':
-            if (isset($_GET['id']) && Validator::validateNaturalNumber($_GET['id'])) {
-                if ($result['dataset'] = $curso->getCursoByIdCompleto($_GET['id'])) {
-                    $result['status'] = 1;
-                } else {
-                    $result['message'] = 'No se obtuvo los datos del curso';
-                }
-            } else {
-                $result['message'] = 'Datos inválidos';
-            }
-            break;
-        /**
-         * Obtner el curso del empleado asignado
-         */
-        case 'getCursoByIdEmpleado':
-            if (isset($_GET['id']) && Validator::validateNaturalNumber($_GET['id'])) {
-                if ($result['dataset'] = $curso->getCursoByIdEmpleado($_GET['id'])) {
-                    $result['status'] = 1;
-                } else {
-                    $result['message'] = 'No se obtuvo los datos del curso';
+                    $result['message'] = 'No se pudieron obtener los datos del curso';
                 }
             } else {
                 $result['message'] = 'Datos inválidos';
@@ -117,8 +67,7 @@ if (isset($_GET['action'])) {
 
         case 'updateCurso':
             $_POST = Validator::validateForm($_POST);
-            if (
-                isset($_POST['id']) &&
+            if (isset($_POST['id']) &&
                 isset($_POST['nombre']) &&
                 isset($_POST['fechaInicio']) &&
                 isset($_POST['fechaFin']) &&
@@ -127,11 +76,9 @@ if (isset($_GET['action'])) {
                 isset($_POST['programaFormacion']) &&
                 isset($_POST['codigoCurso']) &&
                 isset($_POST['empleado']) &&
-                isset($_POST['estado'])
-            ) {
+                isset($_POST['estado'])) {
 
-                if (
-                    $curso->setId($_POST['id']) &&
+                if ($curso->setId($_POST['id']) &&
                     $curso->setNombre($_POST['nombre']) &&
                     $curso->setFechaInicio($_POST['fechaInicio']) &&
                     $curso->setFechaFin($_POST['fechaFin']) &&
@@ -140,8 +87,7 @@ if (isset($_GET['action'])) {
                     $curso->setProgramaFormacion($_POST['programaFormacion']) &&
                     $curso->setCodigo($_POST['codigoCurso']) &&
                     $curso->setEmpleado($_POST['empleado']) &&
-                    $curso->setEstado($_POST['estado'])
-                ) {
+                    $curso->setEstado($_POST['estado'])) {
 
                     if ($curso->update()) {
                         $result['status'] = 1;
@@ -171,60 +117,10 @@ if (isset($_GET['action'])) {
             }
             break;
 
-        case 'obtenerFechasCurso':
-            if ($result['dataset'] = $curso->obtenerFechasCurso()) {
-                $result['status'] = 1;
-            } else {
-                // Manejar el error y enviar un mensaje adecuado // Código de estado HTTP 500 para errores del servidor
-                $result['error'] = 'Hubo un problema al obtener las fechas del curso: ';
-            }
-            break;
-
-        case 'getCantidadCursosUltimos12Meses':
-            if ($result['dataset'] = $curso->getCantidadCursosUltimos12Meses()) {
-                $result['status'] = 1;
-            } else {
-                // Manejar el error y enviar un mensaje adecuado // Código de estado HTTP 500 para errores del servidor
-                $result['error'] = 'Hubo un problema al obtener los cursos';
-            }
-            break;
-        case 'CursosPorEstado':
-            if ($result['dataset'] = $curso->CursosPorEstado()) {
-                $result['status'] = 1;
-            } else {
-                // Manejar el error y enviar un mensaje adecuado // Código de estado HTTP 500 para errores del servidor
-                $result['error'] = 'Hubo un problema al obtener los cursos';
-            }
-            break;
-        case 'cursosUltimosMesesConProyeccion':
-            if ($result['dataset'] = $curso->cursosUltimosMesesConProyeccion()) {
-                $result['status'] = 1;
-            } else {
-                // Manejar el error y enviar un mensaje adecuado // Código de estado HTTP 500 para errores del servidor
-                $result['error'] = 'Hubo un problema al obtener los datos de la proyeccion';
-            }
-            break;
-        case 'CursosUltimosMeses':
-            if ($result['dataset'] = $curso->CursosUltimosMeses()) {
-                $result['status'] = 1;
-            } else {
-                // Manejar el error y enviar un mensaje adecuado // Código de estado HTTP 500 para errores del servidor
-                $result['error'] = 'Hubo un problema al obtener los datos de la proyeccion';
-            }
-            break;
-        case 'prestamosUltimosMesesConProyeccion':
-            if ($result['dataset'] = $curso->prestamosUltimosMesesConProyeccion()) {
-                $result['status'] = 1;
-            } else {
-                // Manejar el error y enviar un mensaje adecuado // Código de estado HTTP 500 para errores del servidor
-                $result['error'] = 'Hubo un problema al obtener los datos de la proyeccion';
-            }
-            break;
         default:
-            $result['error'] = 'Acción no disponible';
+            $result['message'] = 'Acción no disponible';
     }
-    // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
-    $result['exception'] = Database::getException();
+
     header('Content-Type: application/json; charset=utf-8');
     echo json_encode($result);
 }
